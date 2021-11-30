8ce1a52e1aed0cb903b5f770fdbaf60fff659066
Juuso Alasuutari <juuso.alasuutari@gmail.com>
2021-11-30 13:44:55 +0200

    main

--- main.c
+++ main.c
@@ -20,26 +20,21 @@ int main(void)
 		return EXIT_FAILURE;
 
 	unsigned int i;
-	bad_atomic_t a;
-	pthread_t t[32];
 
-	bad_atomic_init(&a, 0U);
-
-	for (i = 0; i < ARRAY_SIZE(t); ++i) {
-		printf("%2u\n", i);
-		if (pthread_create(&t[i], NULL, f, (void *)&a)) {
+	for (i = 0; i < ctx->num; ++i) {
+		if (pthread_create(&ctx->thd[i], NULL, f, (void *)ctx)) {
 			ctx_destroy(&ctx);
 			return EXIT_FAILURE;
 		}
 	}
 
-	for (i = 0; i < ARRAY_SIZE(t); ++i) {
+	for (i = 0; i < ctx->num; ++i) {
 		void *ret = NULL;
-		pthread_join(t[i], &ret);
+		pthread_join(ctx->thd[i], &ret);
 		printf("%2u\t%08" PRIx32 "\n", i, (uint32_t)(uintptr_t)ret);
 	}
 
-	printf("%016" PRIx64 "\n", atomic_load(bad_atomic_u64(&a)));
+	printf("%016" PRIx64 "\n", atomic_load(bad_atomic_u64(&ctx->ctr)));
 
 	ctx_destroy(&ctx);
 	return EXIT_SUCCESS;
@@ -47,10 +42,10 @@ int main(void)
 
 static void *f(void *arg)
 {
-	bad_atomic_t *a = (bad_atomic_t *)arg;
-	uint64_t i = atomic_fetch_add_explicit(bad_atomic_u64(a), 1U,
+	struct ctx *ctx = (struct ctx *)arg;
+	uint64_t i = atomic_fetch_add_explicit(bad_atomic_u64(&ctx->ctr), 1U,
 	                                       memory_order_release);
-	uint32_t u32 = atomic_fetch_or_explicit(&bad_atomic_u32(a)[1],
+	uint32_t u32 = atomic_fetch_or_explicit(&bad_atomic_u32(&ctx->ctr)[1],
 	                                        UINT32_C(1) << i,
 	                                        memory_order_release);
 	u32 |= UINT32_C(1) << i;
